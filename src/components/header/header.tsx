ae07efc842d33cfb803683c9d97cb42f2303d4f6
tr90814 <tomrogers2823@gmail.com>
2023-06-07 13:49:46 +0100

    Updaate styling

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,11 @@
 import { component$ } from '@builder.io/qwik';
 
 export default component$(() => {
-
   return (
     <header>
+      <div className='profile-image-container'>
+        <img className='profile-image' src='/images/tom.png' alt='Tom Rogers'/>
+      </div>
     </header>
   );
 });
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,16 +4,16 @@ import type { DocumentHead } from '@builder.io/qwik-city';
 export default component$(() => {
   return (
     <>
-      <img className='profile-image' src='/images/tom.png' alt='Tom Rogers'/>
-      <h1>Tom Rogers</h1>
+      <h1 className='profile-name'>Tom Rogers</h1>
 
-      <p>
-        Ex <code>Co-founder</code>, <code>CPTO</code> and <code>NED</code> <a href='https://farewill.com'>@Farewill</a>.
-      </p>
-      
-      <p>
-        Currently <code>advising</code> and <code>contracting</code> with companines in London.
-      </p>
+      <ul>
+        <li>
+          Ex <code>Co-founder</code>, <code>CPTO</code> and <code>NED</code> <a href='https://farewill.com'>@Farewill</a>
+        </li>
+        <li>
+          Ex <code>Head of Tech</code> <a href='https://savemyexams.co.uk'>@SaveMyExams</a>
+        </li>
+      </ul>
     </>
   );
 });
