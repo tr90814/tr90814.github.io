db28f98d80c7af5a0608eea0e8ceb2d100a00793
tr90814 <tomrogers2823@gmail.com>
2023-06-07 13:54:45 +0100

    Tidy code

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useStyles$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
+import styles from './index.css?inline';
 
 export default component$(() => {
+  useStyles$(styles);
+
   return (
     <>
       <h1 className='profile-name'>Tom Rogers</h1>
-
       <ul>
         <li>
           Ex <code>Co-founder</code>, <code>CPTO</code> and <code>NED</code> <a href='https://farewill.com'>@Farewill</a>
